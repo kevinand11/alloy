3ae99f73e8b0c7eadb7bad4c603140fa0e8e0a2f
kevinand11 <kevinfizu@gmail.com>
2026-02-07 09:12:25 +0100

    refactor span to tuple

--- src/span/mod.rs
+++ src/span/mod.rs
@@ -1,11 +1,8 @@
 #[derive(Debug)]
-pub struct Span {
-    pub start: usize,
-    pub end: usize,
-}
+pub struct Span(pub (usize, usize));
 
 impl Span {
     pub const fn from_range(start: usize, end: usize) -> Self {
-        Self { start, end }
+        Self((start, end))
     }
 }
